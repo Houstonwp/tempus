64f9312838c8ffe590aca25955fa82bc08f00f40
Houston Perrett <houstonp@Houstons-iMac.local>
2021-08-06 17:47:36 -0500

    Changed periods to be unsigned integers. The concept of negative periods doesn't make sense. You could subtract periods, but they can't inherently be negative.

--- src/date/period.rs
+++ src/date/period.rs
@@ -1,8 +1,8 @@
-use std::ops::Add;
+use std::ops::{Add, AddAssign, Mul};
 
 
 #[derive(Debug)]
-pub struct Days(i64);
+pub struct Days(u64);
 
 impl Add for Days {
     type Output = Days;
@@ -11,8 +11,21 @@ impl Add for Days {
     }
 }
 
+impl AddAssign for Days {
+    fn add_assign(&mut self, rhs: Self) {
+        Days(self.0 + rhs.0);
+    }
+}
+
+impl Mul<u64> for Days {
+    type Output = Days;
+    fn mul(self, rhs: u64) -> Self::Output {
+        Days(self.0 * rhs)
+    }
+}
+
 #[derive(Debug)]
-pub struct Months(i64);
+pub struct Months(u64);
 impl Add for Months {
     type Output = Months;
     fn add(self, rhs: Self) -> Self::Output {
@@ -20,11 +33,37 @@ impl Add for Months {
     }
 }
 
+impl AddAssign for Months {
+    fn add_assign(&mut self, rhs: Self) {
+        Months(self.0 + rhs.0);
+    }
+}
+
+impl Mul<u64> for Months {
+    type Output = Months;
+    fn mul(self, rhs: u64) -> Self::Output {
+        Months(self.0 * rhs)
+    }
+}
+
 #[derive(Debug)]
-pub struct Years(i64);
+pub struct Years(u64);
 impl Add for Years {
     type Output = Years;
     fn add(self, rhs: Self) -> Self::Output {
         Years(self.0 + rhs.0)
+    }
+}
+
+impl AddAssign for Years {
+    fn add_assign(&mut self, rhs: Self) {
+        Years(self.0 + rhs.0);
+    }
+}
+
+impl Mul<u64> for Years {
+    type Output = Years;
+    fn mul(self, rhs: u64) -> Self::Output {
+        Years(self.0 * rhs)
     }
 }
